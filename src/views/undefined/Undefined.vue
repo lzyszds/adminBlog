f25b81230f96bcb3dff898e53f40f2da85d29468
lzyszds <1024327189@qq.com>
2024-02-05 23:43:29 +0800

    🔧chore：修复undefined界面

--- src/views/undefined/Undefined.vue
+++ src/views/undefined/Undefined.vue
@@ -1,14 +1,30 @@
 <script setup lang='ts'>
-import { useStore } from '@/store';
-
-const state = useStore()
 </script>
 
 <template>
-  <div>
-    404
+  <div class="undefined">
+    内容暂未开发,敬请期待
   </div>
 </template>
 
-<style lang='scss' scoped>
+<style lang='less' scoped>
+@themeColor: var(--themeColor);
+
+.undefined {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  font-size: 50px;
+  color: @themeColor;
+  font-weight: bold;
+  background-color: #fff;
+  flex-direction: column;
+  font-family: 'dindin';
+
+  .undefined {
+    font-size: 20px;
+    color: #000;
+  }
+}
 </style>
